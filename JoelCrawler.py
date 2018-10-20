069dbba49fb011f17c1f11eb7e306d9fbdc70ab7
Joel Schwartz <joelschwartz@mac-air-32.cs.iastate.edu>
2018-10-20 17:47:58 -0500

    gets list of tweets for each user from CNBC

--- JoelCrawler.py
+++ JoelCrawler.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
 import time
+import tweets_scrapper
 
 consumer_key = 'WCqb4vOIcppLOHxJD4Z4q2xwQ'
 consumer_secret = 'kWWChyjsJjeyKZQRUSDs2qnojuBhfWBXxjz11TyfqEdMmcgjEj'
@@ -17,15 +18,34 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
+def BFS(self, s):
+    # Mark all the vertices as not visited
+    visited = [False] * (len(self.graph))
+
+    queue = []
+
+    queue.append(s)
+    visited[s] = True
+
+    while queue:
+        s = queue.pop(0)
+
 
 if __name__ == '__main__':
 
     friends = api.friends_ids("CNBC")
+
     # for i in range(0, len(friends)):
     #     user = api.get_user(friends[i])
     #     print('{} {}'.format(user.screen_name,user.followers_count))
 
+
+
+
+
+
     goodUsers = []
+    goodUsers.append("CNBC")
     i = 1
     index = 0;
     for fr in friends:
@@ -33,40 +53,46 @@ if __name__ == '__main__':
         url = "https://twitter.com/intent/user?user_id=" + str(fr)
         #print(url)
         print(i)
-        if(i % 100 == 0):
+        if(i % 50 == 0):
             print("sleeping")
             time.sleep(60)
             print("continuing")
         content = urlopen(url)
 
         soup = BeautifulSoup(content, 'html.parser')
+        handle = soup.find("span", class_="nickname").string
+        handle = handle[1:]
+        print(handle)
+
         i = i + 1
 
         followers = ""
         verified = ""
-        print(fr)
         try:
             followers = soup.find("a", href=re.compile("/followers")).string
             followers = followers.replace(",", "")
             verified = soup.find("li", class_="verified").string
-            print(verified)
+            #print(verified)
             # print(followers)
         except AttributeError:
             print("error")
             continue
 
         if(int(followers) > 100000 and verified.strip() == "Verified account"):
-            handle = soup.find("span", class_="nickname").string
-            handle = handle[1:]
             #print(handle)
             index = index + 1
             goodUsers.append(handle)
 
-
-        if(index == 12):
+        if(index == 11):
             break
 
     print(goodUsers)
+
+    for user in goodUsers:
+        list = tweets_scrapper.start(user)
+        print(list)
+
+
 
 
 

bb35202f7c98ed08039c01952ca880ee3a4901cc
Joel Schwartz <joelschwartz@mac-air-32.cs.iastate.edu>
2018-10-20 16:05:23 -0500

    crawler starts at CNBC, gets 12 users with >100,000 followers and verified, and adds them to a list with their username

--- JoelCrawler.py
+++ JoelCrawler.py
@@ -1,5 +1,11 @@
+import urllib
+
 import tweepy
 from tweepy import OAuthHandler
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+import time
 
 consumer_key = 'WCqb4vOIcppLOHxJD4Z4q2xwQ'
 consumer_secret = 'kWWChyjsJjeyKZQRUSDs2qnojuBhfWBXxjz11TyfqEdMmcgjEj'
@@ -13,6 +19,54 @@ api = tweepy.API(auth)
 
 
 if __name__ == '__main__':
-    for status in tweepy.Cursor(api.home_timeline).items(10):
-        # Process a single status
-        print(status.text)
+
+    friends = api.friends_ids("CNBC")
+    # for i in range(0, len(friends)):
+    #     user = api.get_user(friends[i])
+    #     print('{} {}'.format(user.screen_name,user.followers_count))
+
+    goodUsers = []
+    i = 1
+    index = 0;
+    for fr in friends:
+
+        url = "https://twitter.com/intent/user?user_id=" + str(fr)
+        #print(url)
+        print(i)
+        if(i % 100 == 0):
+            print("sleeping")
+            time.sleep(60)
+            print("continuing")
+        content = urlopen(url)
+
+        soup = BeautifulSoup(content, 'html.parser')
+        i = i + 1
+
+        followers = ""
+        verified = ""
+        print(fr)
+        try:
+            followers = soup.find("a", href=re.compile("/followers")).string
+            followers = followers.replace(",", "")
+            verified = soup.find("li", class_="verified").string
+            print(verified)
+            # print(followers)
+        except AttributeError:
+            print("error")
+            continue
+
+        if(int(followers) > 100000 and verified.strip() == "Verified account"):
+            handle = soup.find("span", class_="nickname").string
+            handle = handle[1:]
+            #print(handle)
+            index = index + 1
+            goodUsers.append(handle)
+
+
+        if(index == 12):
+            break
+
+    print(goodUsers)
+
+
+
